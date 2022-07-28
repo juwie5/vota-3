7341d8add68a9fa2f9b225c65217fb62754f57a0
unknown <bibire95@gmail.com>
2022-07-28 10:01:06 +0100

    Index page 100% and Organizatio page 100%

--- app.js
+++ app.js
@@ -1,3 +1,35 @@
 function showDropdown() {
-  forEach((document.getElementById("nested")) =>{classList.toggle("show")});
+  let targetElement = document.getElementById("nest");
+  if (targetElement.style.display === "none") {
+    targetElement.style.display = "block";
+  } else {
+    targetElement.style.display = "none";
+  }
+}
+
+function showDropdown1(){
+  let targetElement = document.getElementById("nest1");
+  if(targetElement.style.display === "none"){
+    targetElement.style.display = "block";
+  } else {
+    targetElement.style.display = "none";
+  }
+}
+
+
+function showDropdown2(){
+  let targetElement = document.getElementById("nest2");
+  if(targetElement.style.display === "none"){
+    targetElement.style.display = "block";
+  } else {
+    targetElement.style.display = "none";
+  }
+}
+
+function openModal(){
+  document.getElementById("profile").style.display = "flex";
+}
+
+function closeModal(){
+  document.getElementById("profile").style.display = "none";
 }
